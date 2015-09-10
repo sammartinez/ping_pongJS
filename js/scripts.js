203ded2997a8848ccfd624ca27d837b7c160d946
Sam Martinez <sammartinez19@gmail.com>
2015-09-09 18:51:49 -0700

    able to type in number into webform and check to see if it is a ping or a pong or a pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
 
 var pingPong = function(num) {
-    if(num % 3 === 0 && num % 5 === 0) {
-        return "ping-pong";
-    } else if(num % 3 === 0) {
-        return "ping";
-    } else if(num % 5 === 0) {
-        return "pong";
-    } else {
-        return num;
-    }
+
+        if(num % 3 === 0 && num % 5 === 0) {
+            return "ping-pong";
+        } else if(num % 3 === 0) {
+            return "ping";
+        } else if(num % 5 === 0) {
+            return "pong";
+        } else {
+            return num;
+        }
+
 }
 
-// $(document).ready(function() {
-//     $("form#pingPong").submit(function(event){
-//         var phrase = ($("input#phrase").val());
-//         var result = pingPong(phrase);
-//
-//         $(".answer").text(result);
-//         $("#result").show();
-//         event.preventDefault();
-//     });
-// });
+$(document).ready(function() {
+    $("form#pingPong").submit(function(event){
+        var number = ($("input#number").val());
+        var result = pingPong(number);
+
+        $(".answer").text(result);
+        $("#result").show();
+        event.preventDefault();
+    });
+});
