79aad78ac4b27bb63542291bf9bc4f76d7a3815a
Sam Martinez <sammartinez19@gmail.com>
2015-09-10 12:07:01 -0700

    update files

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 
 var pingPong = function(num) {
 
-        if(num % 3 === 0 && num % 5 === 0) {
-            return "ping-pong";
-        } else if(num % 3 === 0) {
-            return "ping";
-        } else if(num % 5 === 0) {
-            return "pong";
-        } else {
-            return num;
-        }
+        var numArray = [];
 
+    for(var i = 1; i<= num; i++)  {
+        if(i % 3 === 0 && i % 5 === 0) {
+            numArray.push("ping-pong");
+        } else if(i % 3 === 0) {
+            numArray.push("ping");
+        } else if(i % 5 === 0) {
+            numArray.push("pong");
+        } else {
+            numArray.push(i);
+        }
+    }   return numArray;
 }
 
 $(document).ready(function() {
@@ -18,7 +21,7 @@ $(document).ready(function() {
         var number = ($("input#number").val());
         var result = pingPong(number);
 
-        $(".answer").text(result);
+        $(".answer").text(result.join(", "));
         $("#result").show();
         event.preventDefault();
     });
--- spec/specs.js
+++ spec/specs.js
@@ -1,18 +1,15 @@
 
 describe('pingPong', function() {
     it("checks to see if number is divisible by three", function(){
-        expect(pingPong(3)).to.equal("ping");
+        expect(pingPong(3)).to.eql([1, 2,"ping"]);
     });
 
     it("checks to see if number is divisible by five", function(){
-        expect(pingPong(5)).to.equal("pong");
+        expect(pingPong(5)).to.eql([1 , 2,"ping", 4,"pong"]);
     });
 
     it("checks to see if number is divisible by both three and five", function(){
-        expect(pingPong(15)).to.equal("ping-pong");
+        expect(pingPong(15)).to.eql([1 , 2,"ping", 4,"pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"]);
     });
 
-    it("checks to see if number is not divisible by both three and five", function(){
-        expect(pingPong(17)).to.equal(17);
-    });
 });
